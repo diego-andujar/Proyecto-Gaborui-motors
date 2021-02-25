29926db28afa4cfa18d5ebb8b043d549f393f746
Javier Fuenmayor <javierfuenmayor00@gmail.com>
2021-02-24 23:00:30 -0400

    agregand firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDLH5migTXLljPLgoXi6oVCctfUqdhEs0Y",
+    authDomain: "gaborui-motors.firebaseapp.com",
+    projectId: "gaborui-motors",
+    storageBucket: "gaborui-motors.appspot.com",
+    messagingSenderId: "533357849147",
+    appId: "1:533357849147:web:9e66fa0a01470c647a8a1e",
+    measurementId: "G-2X88GC29XZ",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDLH5migTXLljPLgoXi6oVCctfUqdhEs0Y",
+    authDomain: "gaborui-motors.firebaseapp.com",
+    projectId: "gaborui-motors",
+    storageBucket: "gaborui-motors.appspot.com",
+    messagingSenderId: "533357849147",
+    appId: "1:533357849147:web:9e66fa0a01470c647a8a1e",
+    measurementId: "G-2X88GC29XZ",
+  },
 };
 
 /*
