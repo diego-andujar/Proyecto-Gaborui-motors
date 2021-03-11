6e735d9adbf32bec3b215ff251d56c3e059c924d
jhoelblancoo <jhoel.blanco@correo.unimet.edu.ve>
2021-03-10 20:08:40 -0400

    equis2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { QuienesComponent } from './components/quienes/quienes.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MapsComponent } from './components/maps/maps.component';
 import { ClientPageComponent } from './pages/client-page/client-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ManagerPageComponent } from './pages/manager-page/manager-page.component';
@@ -10,6 +13,9 @@ import { FullscreenOverlayContainer } from '@angular/cdk/overlay';
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
+  // { path: "nostros", component: QuienesComponent },
+  // { path: "contact", component: ContactComponent },
+  // { path: "ubicacion", component: MapsComponent },
   { path: "sign-up", component: SignUpPageComponent },
   { path: "log-in", component: LogInPageComponent },
   { path: "manager", component: ManagerPageComponent },
