7f70bbf3c0607fe90afecded15f6636a07652521
Javier <javierfuenmayor00@gmail.com>
2021-03-17 00:07:17 -0400

    creando funciones en el servicio de citas

--- src/app/services/appointment-service.service.ts
+++ src/app/services/appointment-service.service.ts
@@ -13,6 +13,25 @@ export class AppointmentServiceService {
 
   constructor() { }
 
+  getCitasDeterminadas(estado: string): Array<Appointment> {
+    const list: Array<Appointment> = [];
+    const usersRef = this.db.collection('citas')
+    .where("estado", "==", estado).get()
+    .then((querySnapshot) =>{
+      querySnapshot.forEach(doc => {
+        let cita: Appointment = ({
+          car: doc.get("car"),
+          date: doc.get("appointmentDate"),
+          userid: doc.get("userid"),
+          estado: doc.get("estado"),
+          diagnosis: doc.get("diagnosis"),
+        })
+        list.push(cita);
+      })
+    });
+    return list;
+  }
+
   crearCita(appointment: Appointment): Promise<DocumentReference> {
     return this.db.collection("citas").add(appointment);
   }
