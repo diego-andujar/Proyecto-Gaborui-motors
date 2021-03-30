c834423ecc41f85fdda23ce51ae244d051057f3b
Javier <javierfuenmayor00@gmail.com>
2021-03-29 21:53:08 -0400

    calendario manager

--- src/app/components/calendar-manager/calendar-manager.component.ts
+++ src/app/components/calendar-manager/calendar-manager.component.ts
@@ -1,6 +1,8 @@
+import { Appointment } from './../../models/appointment';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Calendar, CalendarOptions, FullCalendarComponent } from '@fullcalendar/angular'; // useful for typechecking
 import esLocale from '@fullcalendar/core/locales/es';
+import { AppointmentServiceService } from 'src/app/services/appointment-service.service';
 
 @Component({
   selector: 'app-calendar-manager',
@@ -11,13 +13,10 @@ export class CalendarManagerComponent implements OnInit {
 
 
 
-
+  calendarEl!: HTMLElement;
+  calendar!: Calendar;
   @ViewChild('calendar') calendarComponent!: FullCalendarComponent;
-  eventsList!: [
-    { title: 'event 1', date: '2021-03-30' },
-    { title: 'event 2', date: '2021-03-31' }
-  ]
-  
+  eventsList!: Array<Appointment>;
   calendarOptions: CalendarOptions = {
     locales: [esLocale],
     timeZone: 'America/Venezuela',
@@ -26,34 +25,60 @@ export class CalendarManagerComponent implements OnInit {
   };
   
 
-  constructor() { 
+  constructor(
+    private appService: AppointmentServiceService,
+  ) { 
   }
 
   ngOnInit(): void {
-    let calendarEl: HTMLElement = document.getElementById('calendar')!;
+    this.calendarEl = document.getElementById('calendar')!;
+    this.calendar = new Calendar(this.calendarEl, {
+      locales: [esLocale],
+      timeZone: 'America/Venezuela',
+      initialView: 'dayGridMonth',
+      weekends: false, 
+      editable: true, // enable draggable events
+      aspectRatio: 1.8,
+      scrollTime: '00:00', // undo default 6am scrollTime
+      views: {
+        resourceTimelineThreeDays: {
+          type: 'resourceTimeline',
+          duration: { days: 3 },
+          buttonText: '3 day'
+        }
+      },
+    });
+    this.llenarLista();
+    this.calendar.render()
+  }
 
-   let calendar = new Calendar(calendarEl, {
-    locales: [esLocale],
-    timeZone: 'America/Venezuela',
-    initialView: 'dayGridMonth',
-    weekends: false, 
-    editable: true, // enable draggable events
-    aspectRatio: 1.8,
-    scrollTime: '00:00', // undo default 6am scrollTime
-    views: {
-      resourceTimelineThreeDays: {
-        type: 'resourceTimeline',
-        duration: { days: 3 },
-        buttonText: '3 day'
-      }
-    },
-  });
+  transformDateForCalendar(date: string): string{
+    const [day, month, year]: string[] = date.split('-');
+    return`${year}-${month}-${day}`
+  }
 
-    calendar.addEvent({ // this object will be "parsed" into an Event Object
-      title: 'The Title', // a property!
-      start: '2021-03-29', // a property!
+  llenarLista(){
+    this.eventsList = [];
+    this.appService.getEnEsperaApp().then( res => {
+      this.eventsList.push(res as Appointment);
     })
-    calendar.render()
+    this.appService.getEnProcesoApp().then( doc => {
+      this.eventsList.push(doc as Appointment);
+    })
+    this.appService.getTerminadaApp().then( list => {
+      this.eventsList.push(list as Appointment);
+    })
+    this.llenarCalendario();
+  }
+
+  llenarCalendario(){
+    this.eventsList.forEach(element => {
+      const date = this.transformDateForCalendar(element.date!);
+      this.calendar.addEvent({ // this object will be "parsed" into an Event Object
+      title: element.carInfo, // a property!
+      start: date, // a property!
+    })
+    });
   }
 
   handleDateClick(arg: any) {
