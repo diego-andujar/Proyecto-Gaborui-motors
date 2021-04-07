dec376dbecef4015f98494e5a8d6c4246a82b3a2
Diego Andujar <diego.andujar@correo.unimet.edu.ve>
2021-04-07 14:27:54 -0400

    new slider y detalles

--- src/app/components/new-car-form/new-car-form.component.ts
+++ src/app/components/new-car-form/new-car-form.component.ts
@@ -64,7 +64,7 @@ export class NewCarFormComponent implements OnInit {
     });
   }
 
-  /*onSubmit(): void {
+  onSubmit(): void {
     const newCar: Car = {
       userid: this.user.uid,
       brand: this.carForm.get("brand")?.value,
@@ -76,13 +76,8 @@ export class NewCarFormComponent implements OnInit {
     }
     this.createNewCar(newCar);
     this.carForm.reset();
-<<<<<<< HEAD
-    alert("¡Felicitaciones su vehiculo fue agregado exitosamente!\nRecargue la pagina para que lo pueda ver");
-  }*/
-=======
     alert("!Se ha creado con exito tu carro!")
   } 
->>>>>>> origin/fuenmayor
 
   createNewCar(newCar: Car): void {
     this.carService.createNewCar(newCar);
